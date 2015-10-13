04e85bd073004efc8a7ea340cef3d399d0226139
spothu <spothu@pureprofile.com>
2015-10-13 20:55:13 +0530

    first commit

---
+++ src/test/java/com/learntestng/SampleTest.java
@@ -0,0 +1,13 @@
+package com.learntestng;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void Sampletest()
+	{
+		System.out.println("**********Sample TEST****** ");
+	}
+
+}
